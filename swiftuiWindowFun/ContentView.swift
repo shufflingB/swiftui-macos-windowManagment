64703c7961ec6b444d500bba755322c31836718a
Jonathan Hume <jonathan.hume@gmail.com>
2022-02-09 18:32:17 +0000

    Initial Commit

---
+++ swiftuiWindowFun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftuiWindowFun
+//
+//  Created by Jonathan Hume on 09/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftuiWindowFun/swiftuiWindowFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiWindowFunApp.swift
+//  swiftuiWindowFun
+//
+//  Created by Jonathan Hume on 09/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiWindowFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
