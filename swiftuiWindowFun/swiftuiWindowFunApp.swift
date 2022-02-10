fca82642b9894c3f3f4ed8f57e49dd14d6d17e71
Jonathan Hume <jonathan.hume@gmail.com>
2022-02-10 17:19:05 +0000

    Pre-adding is key-window

--- swiftuiWindowFun/swiftuiWindowFunApp.swift
+++ swiftuiWindowFun/swiftuiWindowFunApp.swift
@@ -8,10 +8,61 @@
 import SwiftUI
 
 @main
-struct swiftuiWindowFunApp: App {
+struct DemoApp: App {
+    @State var appModel = AppModel()
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        WindowGroup("Main") {
+            MainContentView()
+                .environmentObject(appModel)
+                
         }
+
+        WindowGroup("Window 2") {
+            WindowContentView2()
+                .environmentObject(appModel)
+        }
+        .handlesExternalEvents(matching: [AppModel.GroupId.win2.rawValue])
+
+        WindowGroup("Window 3") {
+            WindowContentView3()
+                .environmentObject(appModel)
+                .handlesExternalEvents(preferring: [AppModel.GroupId.win3.rawValue],
+                                       allowing: [AppModel.GroupId.win3.rawValue])
+        }
+        .handlesExternalEvents(matching: [AppModel.GroupId.win3.rawValue])
+    }
+}
+
+struct MainContentView: View {
+    @EnvironmentObject var appModel: AppModel
+    var body: some View {
+        VStack {
+            Button("Open many Window 2") {
+                appModel.openWindowViaUrl(.win2)
+            }
+
+            Button("Open or raise Window 3") {
+                appModel.openWindowViaUrl(.win3)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct WindowContentView2: View {
+    var body: some View {
+        VStack {
+            Text("Window #2")
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct WindowContentView3: View {
+    var body: some View {
+        VStack {
+            Text("Window #3")
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
